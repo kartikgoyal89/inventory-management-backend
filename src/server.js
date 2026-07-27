851164e9abd3000f2fecddb9ab1792253b2878bf
Kartik Goyal <kartikgoyal794@gmail.com>
2026-07-27 23:15:13 +0530

    add github webhook endpoint

--- src/server.js
+++ src/server.js
@@ -1,10 +1,44 @@
 import dotenv from "dotenv";
-dotenv.config({path: "../.env"});
+dotenv.config();
 
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
+import { spawn } from "child_process";
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
+
+app.get("/github-webhook", (req, res) => {
+  const bashChildProcess = spawn("bash", ["/home/ubuntu/script.sh"]);
+
+  // console.log(bashChildProcess);
+
+  // bashChildProcess.stdout.pipe(process.stdout);
+
+  bashChildProcess.stdout.on("data", (data) => {
+    console.log("Got stdout data");
+    process.stdout.write(data);
+  });
+
+  bashChildProcess.stderr.on("data", (data) => {
+    process.stdout.write(data);
+  });
+
+  bashChildProcess.on("close", (code) => {
+    res.json({ message: "OK" });
+    if (code === 0) {
+      console.log("Script Executed Succesfully!");
+    } else {
+      console.log("Script Failed!");
+    }
+  });
+
+  bashChildProcess.on("error", (err) => {
+    res.json({ message: "OK" });
+
+    console.log("Error in spawning the process.");
+    console.log(err);
+  });
+});
 
 const start = async () => {
   try {
